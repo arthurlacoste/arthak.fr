40ff5f611790757be0a4c224ca69d1290964012b
Arthur Lacoste <arthurlacoste@users.noreply.github.com>
2026-08-13 08:40:51 +0200

    Fix Keystatic Astro runtime

--- admin/astro.config.mjs
+++ admin/astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config'
 import keystatic from '@keystatic/astro'
 import cloudflare from '@astrojs/cloudflare'
+import react from '@astrojs/react'
+import markdoc from '@astrojs/markdoc'
 
 export default defineConfig({
-  integrations: [keystatic()],
+  integrations: [react(), markdoc(), keystatic()],
   output: 'server',
   adapter: cloudflare(),
 })
